4db17f7f9de5550005ede3ad3eee73eec1b8317f
Bhaalu-69 <discordsimpcity+github@gmail.com>
2023-06-29 16:22:24 +0530

    .

--- app.js
+++ app.js
@@ -1,16 +1,51 @@
+const config = require('./config.json');
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static('public'));
+main().catch(err => console.log(err));
 
-app.get('/', (req, res) => {
-  res.status(200).sendFile(__dirname + '/index.html');
-});
+async function main() {
+    await mongoose.connect(config.databaseConnectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
 
-app.post('/');
+    const contactSchema = new mongoose.Schema({
+        fullName: String,
+        email: String,
+        mobileNumber: Number,
+        emailSubject: String,
+        message: String,
+    });
+    const Contact = mongoose.model('Contact', contactSchema);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+    // Serve static files from the "public" directory
+    app.use(express.static('public'));
+    const bodyParser = require('body-parser');
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.get('/', (req, res) => {
+        res.status(200).sendFile(__dirname + '/index.html');
+    });
+
+    app.post('/', async (req, res) => {
+        const { fullName, email, mobileNumber, emailSubject, message } = req.body;
+        console.log(fullName, email);
+        const newContact = new Contact({
+            fullName: fullName,
+            email: email,
+            mobileNumber: mobileNumber,
+            emailSubject: emailSubject,
+            message: message,
+        });
+
+        await newContact.save();
+        res.send("Ok!")
+    });
+
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`);
+    });
+}
