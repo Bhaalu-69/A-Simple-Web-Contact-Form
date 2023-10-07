81be0576cc5ea864ed9a0e67ffb05b5ef363d1ff
Prince Raj <profile.princeraj+github@gmail.com>
2023-10-07 13:16:56 +0530

    Updated dependencies and added support for Node.js 20.8.3

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-# DevProjects - Simple web contact form
-
-This is an open source project from [DevProjects](http://www.codementor.io/projects). Feedback and questions are welcome!
-Find the project requirements here: [Simple web contact form](https://www.codementor.io/projects/web/create-a-contact-form-b2n9ltrdy1)
+# Simple web contact form
 
 ## Tech/framework used
 Built with HTML, CSS and JavaScript.
@@ -23,7 +20,7 @@ Please make sure that you have node.js installed in your system before following
 1. Clone the repository.
 2. Run `npm init -y` on your console.
 3. Run `npm i` on your console.
-4. Change the name of the file `config.json.example` to `config.json` after adding your Mongo DB Connection String inside it at the correct place.
+4. Change the name of the file `config.json.example` to `config.json` after adding your Mongo DB Connection String inside it at the correct place.(Please make sure that you allow database connections from your server using the MongoDB dashboard)
 5. Run `node app.js` on your console.
 
 ## License
--- app.js
+++ app.js
@@ -1,21 +1,28 @@
+// Import necessary modules
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+
+// Middleware to parse form data
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Import configuration file for the database connection
 const config = require('./config.json'); // for databaseConnectionString
 
+// Main function to establish a connection to MongoDB
 main().catch(err => console.log(err));
-let Contact;
+
 async function main() {
+    // Establishes connection to MongoDB
     await mongoose.connect(config.databaseConnectionString, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
 }
 
+// Define the schema for the 'Contact' model
 const contactSchema = new mongoose.Schema({
     fullName: String,
     email: String,
@@ -23,29 +30,39 @@ const contactSchema = new mongoose.Schema({
     optionChosen: String,
     message: String,
 });
-Contact = mongoose.model('Contact', contactSchema);
 
-app.set('view engine', 'pug');
+// Create a 'Contact' model
+let Contact = mongoose.model('Contact', contactSchema);
+
+// Set up the Pug template engine
+app.set('view engine', 'pug'); // Sets PUG as the template engine
 app.set('views', './views');
-app.set(express.static('static'));
 
+// Serve static files from the 'static' directory
+app.use(express.static('static'));
+
+// Define a route for the homepage
 app.get('/', (req, res) => {
-    res.status(200).render('home');
+    res.status(200).render('home'); // Render the 'home' template
 });
 
+// Handle POST request when the form is submitted
 app.post('/', (req, res) => {
-    res.status(200).render('confirm', { 
-        fullNameField : req.body.fullName,
-        emailField : req.body.email,
-        mobileNumberField : req.body.mobileNumber,
-        messageField : req.body.message,
-        optionChosenField : req.body.optionChosen,
-     });
+    res.status(200).render('confirm', {
+        fullNameField: req.body.fullName,
+        emailField: req.body.email,
+        mobileNumberField: req.body.mobileNumber,
+        messageField: req.body.message,
+        optionChosenField: req.body.optionChosen,
+    });
 });
 
-// Taking form-input and saving it to database
+// Handle POST request to save form input to the database
 app.post('/confirm', (req, res) => {
+    // Extract form input data
     const { fullName, email, mobileNumber, optionChosen, message } = req.body;
+
+    // Create a new 'Contact' document
     const newContact = new Contact({
         fullName: fullName,
         email: email,
@@ -54,15 +71,17 @@ app.post('/confirm', (req, res) => {
         message: message,
     });
 
+    // Save the new 'Contact' document to the database
     newContact.save()
         .then(() => {
-            res.status(200).render('succesful');
+            res.status(200).render('succesful'); // Render the 'successful' template
         })
         .catch(error => {
             console.log(error);
         });
 });
 
+// Start the Express server
 app.listen(port, () => {
     console.log(`Website live on port ${port}`);
 });
