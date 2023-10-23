77733e686bb5ccee63ce36cbd221a443e6b8595c
Prince Raj <profile.princeraj+github@gmail.com>
2023-10-24 01:02:35 +0530

    Finishing up

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Import necessary modules
-const mongoose = require('mongoose');
+const firebase = require('./firebase-credentials.js');
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -7,32 +7,6 @@ const bodyParser = require('body-parser');
 
 // Middleware to parse form data
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Import configuration file for the database connection
-const config = require('./config.json'); // for databaseConnectionString
-
-// Main function to establish a connection to MongoDB
-main().catch(err => console.log(err));
-
-async function main() {
-    // Establishes connection to MongoDB
-    await mongoose.connect(config.databaseConnectionString, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-}
-
-// Define the schema for the 'Contact' model
-const contactSchema = new mongoose.Schema({
-    fullName: String,
-    email: String,
-    mobileNumber: Number,
-    optionChosen: String,
-    message: String,
-});
-
-// Create a 'Contact' model
-let Contact = mongoose.model('Contact', contactSchema);
 
 // Set up the Pug template engine
 app.set('view engine', 'pug'); // Sets PUG as the template engine
@@ -48,37 +22,41 @@ app.get('/', (req, res) => {
 
 // Handle POST request when the form is submitted
 app.post('/', (req, res) => {
+    // Render the 'confirm' template with form data
     res.status(200).render('confirm', {
         fullNameField: req.body.fullName,
         emailField: req.body.email,
         mobileNumberField: req.body.mobileNumber,
-        messageField: req.body.message,
         optionChosenField: req.body.optionChosen,
+        messageField: req.body.message,
     });
 });
 
 // Handle POST request to save form input to the database
 app.post('/confirm', (req, res) => {
+
     // Extract form input data
     const { fullName, email, mobileNumber, optionChosen, message } = req.body;
 
-    // Create a new 'Contact' document
-    const newContact = new Contact({
+    const locationInDatabase = firebase.database().ref('FormEntries');
+    const newEntryRef = locationInDatabase.push(); // Generate a new unique key
+
+    let entryData = {
         fullName: fullName,
         email: email,
         mobileNumber: mobileNumber,
         optionChosen: optionChosen,
         message: message,
-    });
+    };
 
-    // Save the new 'Contact' document to the database
-    newContact.save()
-        .then(() => {
-            res.status(200).render('succesful'); // Render the 'successful' template
-        })
-        .catch(error => {
-            console.log(error);
-        });
+    // Save the form data to Firebase database
+    newEntryRef.set(entryData, (err) => {
+        if (!err) {
+            res.status(200).render('successful'); // Render the 'successful' template
+        } else {
+            console.error('\nError writing to Firebase: ', err);
+        }
+    });
 });
 
 // Start the Express server
